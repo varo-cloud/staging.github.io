bf26eed20306ac1394483ae568f1b011df4c7454
AILINGANGEL <AILINGANGEL@users.noreply.github.com>
2026-06-24 06:16:30 +0000

    deploy: varo-cloud/varo-cloud.github.io@11c4d3b03fa31d0fa46ff672111bb1e60906edac

---
+++ assets/TermsView-DlwuRfOx.js
@@ -0,0 +1,1 @@
+import{C as e,R as t,n,ut as r,v as i}from"./vue-i18n-DMHcr8bP.js";import{t as a}from"./PagePlaceholder-Cl4SdEzl.js";var o=e({__name:`TermsView`,setup(e){let{t:o}=n();return(e,n)=>(t(),i(a,{title:r(o)(`pages.terms.title`),description:r(o)(`pages.terms.placeholder`)},null,8,[`title`,`description`]))}});export{o as default};
